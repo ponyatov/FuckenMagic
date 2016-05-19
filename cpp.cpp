e7d285265a3aeb083e387fa4328d366c9c6e0fcb
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-19 13:10:37 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cpp.cpp
@@ -0,0 +1,11 @@
+#include "hpp.hpp"
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main() { return yyparse(); }
+
+Sym::Sym(string V) { val=V; }
+void Sym::push(Sym*o) { nest.push_back(o); }
+string Sym::head() { return "<"+val+">"; }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	return S; }
---
+++ hpp.hpp
@@ -0,0 +1,28 @@
+#ifndef _H_FM
+#define _H_FM
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {
+	string val;
+	Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string head(); string pad(int);
+	virtual string dump(int=0);
+};
+
+struct List: Sym { List(); };
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_FM
